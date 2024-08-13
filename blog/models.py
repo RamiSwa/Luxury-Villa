cf5a1438a195973238713f869b2f79f890b0fe8a
RamiSwa <118728140+RamiSwa@users.noreply.github.com>
2024-08-13 12:26:35 +0300

    add the full blog

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
@@ -25,7 +26,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title  
     
-    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
 
 
 class Category(models.Model):
